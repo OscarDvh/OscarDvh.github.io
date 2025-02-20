457121c493ae4e0c6e14785570758114ace9812f
OscarDvh <oscardanielvh@gmail.com>
2025-02-20 14:07:47 -0600

    tailwind config edit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,17 @@
-import animations from '@midudev/tailwind-animations'
+import animations from '@midudev/tailwind-animations';
+import tailwindScrollbar from 'tailwind-scrollbar';
 
-/** @type {import('tailw	indcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		animations,
-		require('tailwind-scrollbar'),
-	],
-	colors: {
-		'brownb': '#D9D9D9',
-	}
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    animations,
+    tailwindScrollbar
+  ],
+  colors: {
+    'brownb': '#D9D9D9',
+  }
+};
